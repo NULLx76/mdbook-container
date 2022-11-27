a5df1afb0d2accc9d47bcbc4c1a4effe741940bd
victor <victor@xirion.net>
2022-11-27 21:55:46 +0100

    testing flake-based container

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM alpine:3 as builder
-RUN apk add --no-cache cargo
-RUN cargo install mdbook
-RUN cargo install mdbook-toc
-
-FROM alpine:3
-RUN apk add --no-cache libgcc
-COPY --from=builder /root/.cargo/bin/mdbook /bin/mdbook
-COPY --from=builder /root/.cargo/bin/mdbook-toc /bin/mdbook-toc
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "mdbook-plus";
+            contents = mdbook-toc;
+            config.Cmd = [ "${pkgs.mdbook}/bin/mdbook" ];
+          };
+          mdbook-toc = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "mdbook-toc";
+            version = "0.10.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "badboy";
+              repo = pname;
+              rev = version;
+              sha256 = "sha256-/MPxuzqgWKyLP1fn/WoA2cKlV0TiVidt+YpNqY96sxE=";
+            };
+
+            cargoSha256 = "sha256-JFqkeea6o5Wzpe+2Fp+UYjBOh/e3vX1pZzCfxxfAYmU=";
+          };
+        };
+      });
+}
