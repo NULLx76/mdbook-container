8e9413786ac670b5530343846b46e1f9f4cf5235
Victor <victor@xirion.net>
2023-09-08 13:43:04 +0200

    add katex

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "an container for mdbook with the toc plugin";
+  description = "an container for mdbook with various plugins";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -11,29 +11,30 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages = rec {
-          default = mdbook-toc;
+          default = container;
           container = pkgs.dockerTools.buildLayeredImage {
             name = "mdbook-container";
+            tag = "0.4.0";
             contents = [
               (pkgs.buildEnv {
                 name = "mdbook-env";
-                paths = with pkgs; [ busybox mdbook mdbook-toc ];
+                paths = with pkgs; [ busybox mdbook mdbook-toc mdbook-katex ];
               })
             ];
             config.Cmd = [ "${pkgs.mdbook}/bin/mdbook" ];
           };
           mdbook-toc = pkgs.rustPlatform.buildRustPackage rec {
             pname = "mdbook-toc";
-            version = "0.11.0";
+            version = "0.14.1";
 
             src = pkgs.fetchFromGitHub {
               owner = "badboy";
               repo = pname;
               rev = version;
-              sha256 = "sha256-ORJV2+Uh8GwXU+EWUQ2ls+AcplYbpYhl6hvCuFdKpTk=";
+              sha256 = "sha256-F0dIqtDEOVUXlWhmXKPOaJTEuA3Tl3h0vaEu7VsBo7s=";
             };
 
-            cargoSha256 = "sha256-s+xlrHaynHTMmm7rfjYrWNlIJRHO0QTjMlcV+LjqHNs=";
+            cargoSha256 = "sha256-gbBX6Hj+271BA9FWmkZdyR0tMP2Lny7UgW0o+kZe9bU=";
           };
         };
       });
